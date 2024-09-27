da26d28a81e772041673a8834b07e2b7db8c0dd3
Yernur <shalgymbayev.yernur2001@gmail.com>
2024-09-27 14:12:25 +0500

    alfa-1.0.2 - add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,32 @@
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	const handleCloseWebView = () => {
+		try {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.closeWebView) {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				window.webkit.messageHandlers.closeWebView.postMessage("closeWebView");
+			} else {
+				console.log("No native listeners found");
+			}
+		} catch (e) {
+			console.log("Error calling native handler:", e);
+		}
+	};
+
+	return (
+		<div className="min-h-screen bg-black text-white grid place-items-center">
+			<div className="px-6 py-4 flex flex-col gap-2 justify-center items-center bg-gray-900 rounded-md">
+				<h4 className="text-lg font-medium">Damir Chert</h4>
+				<h4 className="text-lg font-medium">Nurzhan Lox</h4>
+				<button
+					className="bg-white text-black font-bold tracking-wide uppercase px-4 py-2 rounded-md hover:bg-slate-200"
+					onClick={handleCloseWebView}
+				>
+					Click me
+				</button>
+			</div>
+		</div>
+	);
 }
